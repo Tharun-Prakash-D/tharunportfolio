f2838b86f4538c4f895315472f07aca3333ff034
Tharun-Prakash-D <92618719+Tharun-Prakash-D@users.noreply.github.com>
2023-08-18 08:45:37 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,26 @@
+let about = document.getElementById('about');
+about.addEventListener('click',()=>{
+    window.location="/new portfolio/portfolio.html#aboutme";
+})
+let techstack = document.getElementById('stack');
+techstack.addEventListener('click',()=>{
+    window.location="/new portfolio/portfolio.html#tech"
+})
+let project = document.getElementById('pro');
+project.addEventListener('click',()=>{
+    window.location="/new portfolio/portfolio.html#proj"
+})
+let contact = document.getElementById('cont');
+contact.addEventListener('click',()=>{
+    window.location="/new portfolio/portfolio.html#conta"
+})
+let xogame = document.getElementById('xoredirect')
+xogame.addEventListener('click',()=>{
+    console.log("Hi")
+    window.location="https://tharun-prakash-d.github.io/xo/"
+})
+let cargame = document.getElementById('carredirect')
+cargame.addEventListener('click',()=>{
+    console.log("Hi")
+    window.location="https://tharun-prakash-d.github.io/game/"
+})
